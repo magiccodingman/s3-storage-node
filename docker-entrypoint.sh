7c4263e09b6124d61601665bcf5ecbd5842e86db
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-08-03 23:27:45 -0400

    fix: dispatch operator CLI arguments from the image entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 set -eu
+
+if [ "$#" -gt 0 ]; then
+  exec python3 -m s3_storage_node.main "$@"
+fi
+
 exec python3 -m s3_storage_node.main run --config "${S3_STORAGE_NODE_CONFIG:-/etc/s3-storage-node/config.toml}"
